3ea7a4de068ab4becd1830c87a8dcdfde66d2ffc
YehorSaz <inercoin@gmail.com>
2023-02-01 02:38:46 +0200

    js homework lesson1

--- lesson1/scripts/main.js
+++ lesson1/scripts/main.js
@@ -48,9 +48,9 @@ console.log(person);
 let a = 100;
 let b = '100';
 let c = true;
-console.log(typeof (a));
-console.log(typeof (b));
-console.log(typeof (c));
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
 
 
 let visitor;
